144c30d5d14f258db292908fd151857a6b0bdd67
romauov <l2omanov@ya.ru>
2024-09-24 09:27:00 +0400

    timezone issue and exercise list

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from app.utils import set_schedule
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    scheduler = AsyncIOScheduler(timezone="Europe/Samara")
+    scheduler = AsyncIOScheduler(timezone="UTC")
     
     bot = Bot(token=s.tg_token)
     dp = Dispatcher()
