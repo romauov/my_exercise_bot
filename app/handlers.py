b311cd92b96137d585b3394881f3cf5cac4840fc
romauov <l2omanov@ya.ru>
2024-10-11 16:21:45 +0400

    weight tracker added

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,45 @@
-from aiogram import types, Router
-from aiogram.filters import CommandStart
-
+from aiogram import Bot, types, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.filters import CommandStart, Command
+from aiogram.types import FSInputFile, Message
+from datetime import datetime
+from app.utils import draw_plot, save_weight_json
 
 router = Router()
+
+class WeightUpdate(StatesGroup):
+    model = State()
+
+
 @router.message(CommandStart())
 async def send_welcome(message: types.Message):
     await message.answer("Привет! Я бот, который отправляет сообщения по расписанию.")
+
+@router.message(Command('weight'))
+async def update_weight(message: Message, state: FSMContext):
+    await state.set_state(WeightUpdate.model)
+    await message.answer("Введи вес в формате: 99.9")
+
+@router.message(Command('weight_full'))
+async def send_weight(message: Message, state: FSMContext):
+    draw_plot(user_id=message.from_user.id, period=0)
+    photo_file = FSInputFile(path='plot.png')
+    await message.answer_photo(photo=photo_file)
     
+@router.message(WeightUpdate.model)
+async def save_weight(message: Message, state: FSMContext, bot: Bot):
+    try:
+        weight = float(message.text)
+        user_id = message.from_user.id
+        date = datetime.now().strftime('%d-%m-%Y')
+
+        save_weight_json(user_id, weight, date)
+
+        await message.reply("Вес сохранен!")
+        await state.clear()
+        photo_file = FSInputFile(path='plot.png')
+        await message.answer_photo(photo=photo_file)
+        
+    except Exception as e:
+        await message.reply(e)
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,8 @@
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 import random
+import pandas as pd
 from datetime import datetime
 from aiogram import Bot
 from apscheduler.triggers.cron import CronTrigger
@@ -36,3 +39,45 @@ def set_schedule(scheduler, bot):
                               kwargs={'bot': bot})
 
     scheduler.start()
+
+def draw_plot(user_id, period=14):
+    with open(f'{user_id}_weights.json', 'r') as file:
+                json_data = json.load(file)
+
+    df = pd.DataFrame(json_data)
+    if period != 0:
+        last_records = df.tail(period)
+        title = f'Вес за последние {period} дней'
+    else:
+        last_records = df
+        title = 'Вес за всё время'
+    avg_weight = round(np.nanmean(last_records['weight']), 2)
+    plt.figure(figsize=(10, 5))
+    plt.plot(last_records['date'], last_records['weight'], 'o-r', label=avg_weight )
+    plt.axhline(y=avg_weight)
+    plt.xlabel('Дата')
+    plt.ylabel('Вес')
+    plt.title(title)
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig('plot.png')
+    plt.close()
+     
+def save_weight_json(user_id, weight, date):
+    data = {
+        "weight": weight,
+        "date": date
+    }
+    try:
+        with open(f'{user_id}_weights.json', 'r') as file:
+            weights = json.load(file)
+    except FileNotFoundError:
+        weights = []
+
+    weights.append(data)
+
+    with open(f'{user_id}_weights.json', 'w') as file:
+        json.dump(weights, file, indent=4)
+    draw_plot(user_id=user_id)
